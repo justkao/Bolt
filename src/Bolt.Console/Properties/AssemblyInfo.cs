cb1e9c351435600fecd86425680b5a8b1149c7c6
justkao <tomka.martin@gmail.com>
2014-12-15 23:11:49 +0100

    assembly title changed

--- src/Bolt.Console/Properties/AssemblyInfo.cs
+++ src/Bolt.Console/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bolt.Console")]
+[assembly: AssemblyTitle("Bolt.Tool")]
 [assembly: AssemblyDescription("Bolt code generation tool used by client and server.")]
