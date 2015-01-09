c568746ea6c8b0edcff19b8fc8b78f284af8f4df
justkao <tomka.martin@gmail.com>
2015-01-09 13:38:25 +0100

    - increased bolt version

--- src/Bolt.Helpers/JsonExceptionSerializer.cs
+++ src/Bolt.Helpers/JsonExceptionSerializer.cs
@@ -6,27 +6,28 @@ namespace Bolt.Helpers
 {
     public class JsonExceptionSerializer : ExceptionSerializerBase<string>
     {
-        private readonly JsonSerializerSettings _exceptionSerializerSettings = new JsonSerializerSettings
-        {
-            TypeNameAssemblyFormat = FormatterAssemblyStyle.Full,
-            TypeNameHandling = TypeNameHandling.All,
-            Formatting = Formatting.None,
-            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-        };
-
         public JsonExceptionSerializer(ISerializer serializer)
             : base(serializer)
         {
+            ExceptionSerializerSettings = new JsonSerializerSettings
+                                              {
+                                                  TypeNameAssemblyFormat = FormatterAssemblyStyle.Full,
+                                                  TypeNameHandling = TypeNameHandling.All,
+                                                  Formatting = Formatting.None,
+                                                  ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                                              };
         }
+
+        public JsonSerializerSettings ExceptionSerializerSettings { get; private set; }
 
         protected override string CreateDescriptor(Exception exception)
         {
-            return JsonConvert.SerializeObject(exception, _exceptionSerializerSettings);
+            return JsonConvert.SerializeObject(exception, ExceptionSerializerSettings);
         }
 
         protected override Exception CreateException(string descriptor)
         {
-            return JsonConvert.DeserializeObject<Exception>(descriptor, _exceptionSerializerSettings);
+            return JsonConvert.DeserializeObject<Exception>(descriptor, ExceptionSerializerSettings);
         }
     }
 }
