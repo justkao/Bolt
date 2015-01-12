a48ed2f5d2ea96d0d052eecefce972ae1bb7958e
justkao <tomka.martin@gmail.com>
2015-01-12 13:12:15 +0100

    - Bolt will use semantic versioning from now

--- src/Bolt.Core/Properties/SharedAssemblyInfo.cs
+++ src/Bolt.Core/Properties/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("Martin Tomka")]
 [assembly: AssemblyProduct("Bolt")]
-[assembly: AssemblyVersion("0.9.1.1")]
-[assembly: AssemblyFileVersion("0.9.1.1")]
+[assembly: AssemblyVersion("0.10.0")]
+[assembly: AssemblyFileVersion("0.10.0")]
