6df6e6ab5c8af7f9d2f0ef80dd8fcd715072fdec
justkao <tomka.martin@gmail.com>
2015-01-12 13:26:47 +0100

    - use prelease versioning

--- src/Bolt.Core/Properties/SharedAssemblyInfo.cs
+++ src/Bolt.Core/Properties/SharedAssemblyInfo.cs
@@ -3,4 +3,5 @@
 [assembly: AssemblyCompany("Martin Tomka")]
 [assembly: AssemblyProduct("Bolt")]
 [assembly: AssemblyVersion("0.10.0")]
+[assembly: AssemblyInformationalVersion("0.10.0-alpha")]
 [assembly: AssemblyFileVersion("0.10.0")]
