fb2f403a043f8d313a088b89ab87710ec805908f
tomka.martin@gmail.com <tomka.martin@gmail.com>
2015-06-17 23:43:24 +0200

    - build commands updated

--- test/ExecuteTests.cmd
+++ test/ExecuteTests.cmd
@@ -1,9 +1,13 @@
 @ECHO OFF
 
-call dnu restore Automatic\Bolt.Core.Test
 call dnx Automatic\Bolt.Core.Test\project.json test
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-call dnu restore Integration\Bolt.Server.IntegrationTest
+call dnx Automatic\Bolt.Client.Test\project.json test
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+call dnx Automatic\Bolt.Server.Test\project.json test
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 call dnx Integration\Bolt.Server.IntegrationTest\project.json test
 if %errorlevel% neq 0 exit /b %errorlevel%
