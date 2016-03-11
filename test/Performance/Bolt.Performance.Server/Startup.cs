71b5e23fc67f1f877cf73d01a13a458904089a6d
kao <tomka.martin@gmail.com>
2016-03-11 21:29:37 +0100

    Build fix

--- test/Performance/Bolt.Performance.Server/Startup.cs
+++ test/Performance/Bolt.Performance.Server/Startup.cs
@@ -3,37 +3,53 @@ using Bolt.Server;
 using Bolt.Performance.Contracts;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Hosting.Internal;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Bolt.Performance.Server
 {
-    public class Startup
+    public class Program
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public class Startup
         {
-            System.Console.WriteLine("Process: {0}", System.Diagnostics.Process.GetCurrentProcess().Id);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            public void ConfigureServices(IServiceCollection services)
+            {
+                System.Console.WriteLine("Process: {0}", System.Diagnostics.Process.GetCurrentProcess().Id);
 
-            ThreadPool.SetMinThreads(100, 100);
-            ThreadPool.SetMinThreads(1000, 1000);
+                ThreadPool.SetMinThreads(100, 100);
+                ThreadPool.SetMinThreads(1000, 1000);
 
-            services.AddLogging();
-            services.AddOptions();
-            services.AddBolt();
-        }
+                services.AddLogging();
+                services.AddOptions();
+                services.AddBolt();
+            }
 
-        public void Configure(IApplicationBuilder app)
-        {
-            // app.ApplicationServices.GetRequiredService<ILoggerFactory>().AddConsole(minLevel: LogLevel.Debug);
+            public void Configure(IApplicationBuilder app)
+            {
+                // app.ApplicationServices.GetRequiredService<ILoggerFactory>().AddConsole(minLevel: LogLevel.Debug);
 
-            app.UseBolt(
-                b =>
-                {
-                    b.Use<IPerformanceContract, PerformanceContractImplementation>();
-                });
+                app.UseBolt(
+                    b =>
+                    {
+                        b.Use<IPerformanceContract, PerformanceContractImplementation>();
+                    });
+            }
         }
 
         // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+        public static int Main(string[] args)
+        {
+            IHostingEngine server =
+                new WebHostBuilder()
+                    .UseServer("Microsoft.AspNet.Server.Kestrel")
+                    .UseStartup<Startup>()
+                    .Build();
+
+            IApplication app = server.Start();
+            System.Console.WriteLine("Server running ... ");
+            app.Dispose();
+            return 0;
+        }
     }
 }
