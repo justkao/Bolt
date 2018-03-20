6e3ac91a3ac77530641ea3c0421db4ff5c04e41d
Martin Tomka <martin.tomka@infor.com>
2018-03-20 13:32:08 +0100

    Drop buffered serializer

--- test/Integration/Bolt.Server.IntegrationTest/BufferedSerializationTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bolt.Serialization;
-
-namespace Bolt.Server.IntegrationTest
-{
-    public class BufferedSerializationTest : SerializationTest
-    {
-        protected override ISerializer CreateSerializer() => new BufferedSerializer(new JsonSerializer());
-    }
-}
